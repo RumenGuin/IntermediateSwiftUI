47c3434fed9c47583a8c300b940e268168a381de
Rumen Guin <rumen2k@gmail.com>
2021-12-15 10:33:47 +0530

    Initial Commit

---
+++ IntermediateSwiftUI/IntermediateSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntermediateSwiftUIApp.swift
+//  IntermediateSwiftUI
+//
+//  Created by RUMEN GUIN on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct IntermediateSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
